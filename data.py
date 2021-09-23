96ef16702b40fc12dc8e6852e373d45e0797ba98
minerva <minerva.suvanto@gmail.com>
2021-09-23 14:31:03 +0200

    working on part 1

---
+++ data.py
@@ -0,0 +1,7 @@
+
+# 1. open folder, loop through files, check if file in training set
+
+# 2. Divide up the official training data files into your own training, validation, 
+# and test datasets depending on your own preferences. 
+
+# Find the corresponding bounding box information in train.jsonl for each image. 
